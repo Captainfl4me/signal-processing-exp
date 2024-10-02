86a76faa228916ec04a381e589263b570215ed17
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2024-10-02 18:28:44 +0200

    fix script + gitignore

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,5 +1,6 @@
-jupyter nbconvert --clear-output --to notebook --output=temp.ipynb
-python ./script/add_temp_toc.py ./temp.ipynb
+jupyter nbconvert ./Compte_rendu_TP.ipynb --clear-output --to notebook --output=temp.ipynb
+python ./scripts/add_temp_toc.py ./temp.ipynb
 jupyter nbconvert --execute --to pdf ./temp.ipynb --LatexPreprocessor.title="TP: Réponse impulsionnelle de salle" --LatexPreprocessor.author_names="Nicolas THIERRY"
 rm ./temp.ipynb
+rm -r ./temp/
 
