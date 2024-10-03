b523215008aa7824d5794c98075f91e5313df84f
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2024-10-03 08:57:18 +0200

    final update

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,5 +1,5 @@
 jupyter nbconvert ./Compte_rendu_TP.ipynb --clear-output --to notebook --output=temp.ipynb
 python ./scripts/add_temp_toc.py ./temp.ipynb
-jupyter nbconvert --execute --to pdf ./temp.ipynb --LatexPreprocessor.title="TP: Réponse impulsionnelle de salle" --LatexPreprocessor.author_names="Nicolas THIERRY" --output=Compte_rendu_TP.pdf
+jupyter nbconvert --execute --to pdf ./temp.ipynb --LatexPreprocessor.title="TP: Réponse impulsionnelle de salle" --LatexPreprocessor.author_names="Nicolas THIERRY & Adrien GUYOT" --output=Compte_rendu_TP.pdf
 rm ./temp.ipynb
 
